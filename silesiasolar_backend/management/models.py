f89c49b93f09fe0885a4feb5a72f2885d1b76d9f
qbic22562 <mateusz.kubica99@gmail.com>
2020-05-12 18:30:25 +0200

    Created new models

--- silesiasolar_backend/management/models.py
+++ silesiasolar_backend/management/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
-from .utils import DataTypes, FunctionCodes
+from .utils import DataTypes, FunctionCodes, MeterTypes, MeasurementTypes
 
 zip_code_regex = '^[0-9]{2}-[0-9]{3}$'
 ip_regex = '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
@@ -21,29 +21,42 @@ class Location(models.Model):
 
 
 class Meter(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    type = models.PositiveIntegerField(choices=MeterTypes.choices(), null=False)
+
+
+class Measurement(models.Model):
+    measurement = models.CharField(max_length=64, null=False, primary_key=True)
+    type = models.PositiveIntegerField(choices=MeasurementTypes.choices(), null=False)
+
+
+class Host(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    meter = models.ForeignKey(Meter, on_delete=models.CASCADE)
 
-    # host = models.CharField(max_length=15, null=False, validators=[RegexValidator(regex=ip_regex)])
-    host = models.CharField(max_length=64, null=False)
+    ip = models.CharField(max_length=64, null=False)
     port = models.PositiveIntegerField(null=False)
     slave_address = models.PositiveSmallIntegerField(null=False)
-
-    meter_type = models.CharField(max_length=32, unique=False)
-    assigned = models.BooleanField(default=False)
     description = models.CharField(max_length=128)
 
     def __str__(self):
-        return '{0}:{1}:{2}'.format(self.host, self.port, self.slave_address)
+        return '{0}:{1}:{2}'.format(self.ip, self.port, self.slave_address)
 
     class Meta:
-        unique_together = ('host', 'port', 'slave_address', )
+        unique_together = ('ip', 'port', 'slave_address', )
+
+
+class ChosenMeasurements(models.Model):
+    host = models.ForeignKey(Host, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
+
 
 class Register(models.Model):
-    # TODO: try to apply many to many relation between meter and register models
-    meter_type = models.CharField(max_length=32, unique=False)
+    meter = models.ForeignKey(Meter, on_delete=models.CASCADE)
+    measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
+
     address = models.PositiveIntegerField(null=False)
-    measurement = models.CharField(max_length=64, null=False)
     unit = models.CharField(max_length=8, blank=True, null=True)
     type = models.PositiveIntegerField(choices=DataTypes.choices(), null=False)
     function_code = models.PositiveIntegerField(choices=FunctionCodes.choices(), null=False)
